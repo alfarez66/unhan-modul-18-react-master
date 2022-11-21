3d9373a89cc9b9c7a91a9b12b5b8ccad01986567
alfarez66 <79447464+alfarez66@users.noreply.github.com>
2022-11-22 00:39:08 +0700

    update 2.0

--- src/pages/not-found.js
+++ src/pages/not-found.js
@@ -3,10 +3,12 @@ import React from 'react'
 // Modifikasi halaman berikut untuk menjadi halaman 404 yang menarik!
 const NotFound = () => {
   return (
-    <div>
-      <p>Hilang arah tanpa tujuan</p>
-      <p>Kami menemukan anda ada pada lokasi <span>404</span></p>
-      <p>itu berarti anda <span>tersesat</span></p>
+    <div className='NotFound'>
+      <div className='contentNotFound'>
+        <p>Hilang arah tanpa tujuan</p>
+        <p>Kami menemukan anda ada pada lokasi <span>404</span></p>
+        <p>itu berarti anda <span>tersesat</span></p>
+      </div>
     </div>
   )
 }
